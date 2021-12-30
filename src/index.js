f4726bb778ed40d370e8da21701d5d4c924e0a0a
Julian Andres Mendez Romero <julian.mendez@grupomok.com>
2021-12-30 11:06:27 -0500

    Se cambia BrowseRouter por HashRouter para solucionar compatibilidad de rutas en gh pages

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/enersinc-front">
+    <HashRouter >
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
